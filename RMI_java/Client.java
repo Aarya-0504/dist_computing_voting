8bfcd78889c2e6ccdd5a86d607d69a712647a65b
nikhil <nikhilprajapati6509@gmail.com>
2024-02-12 22:31:05 +0530

    22:31

--- RMI_java/Client.java
+++ RMI_java/Client.java
@@ -1,25 +1,3 @@
-// import java.rmi.registry.LocateRegistry;
-// import java.rmi.registry.Registry;
-
-// public class Client {
-//     public static void main(String[] args) {
-//         try {
-//             // Get the registry
-//             Registry registry = LocateRegistry.getRegistry(null);
-
-//             // Look up the remote object
-//             Hello stub = (Hello) registry.lookup("Hello");
-
-//             // Call the remote method
-//             String response = stub.sayHello();
-//             System.out.println("Response from server: " + response);
-//         } catch (Exception e) {
-//             System.err.println("Client exception: " + e.toString());
-//             e.printStackTrace();
-//         }
-//     }
-// }
-
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
--- RMI_java/Hello.java
+++ RMI_java/Hello.java
@@ -1,13 +1,3 @@
-// import java.rmi.Remote;
-// import java.rmi.RemoteException;
-
-// // Remote interface
-// public interface Hello extends Remote {
-//     // Remote method declaration
-//     String sayHello() throws RemoteException;
-// }
-
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
